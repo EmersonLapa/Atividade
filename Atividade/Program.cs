9a305690c74b7f14bc990d9ad5276746e27bd401
Emerson <emersonlapa@outlook.com>
2020-10-25 21:43:51 -0300

    Add Frase muito bom

---
+++ Atividade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] entrada1 = { "a", "e", "i", "o", "u" };
+            string[] entrada2 = { "a", "e", "a", "e", "a" };
+            string[] entrada3 = { "a", "b", "c", "d", "e" };
+            for (int i = 0; i < entrada2.Length; i++)
+            {
+                for (int j = 0; j < entrada2.Length; j++)
+                {
+                    Console.WriteLine(entrada2.Length);
+                }
+            }
+            Console.WriteLine("Muito bom !");
+        }
+    }
+}   
+    
+
